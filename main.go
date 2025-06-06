3dd56191efd5dee2e0545adf7c7895e293cba603
Eunice <eunice.chen@conyft.com>
2025-06-06 16:51:58 +0800

    fix build 7

--- main.go
+++ main.go
@@ -16,20 +16,23 @@ func main() {
 	testData := []byte("Hello, SGX Sealing!")
 
 	// Calculate sealed data size
-	sealedSize := C.get_sealed_data_size(C.size_t(len(testData)))
-	if sealedSize == 0 {
-		log.Fatal("Failed to calculate sealed data size")
+	var sealedSize C.uint32_t
+	ret := C.get_sealed_data_size(
+		(*C.uint8_t)(unsafe.Pointer(&testData[0])),
+		C.uint32_t(len(testData)),
+		&sealedSize,
+	)
+	if ret != 0 {
+		log.Fatalf("Failed to calculate sealed data size: %d", ret)
 	}
 
 	// Allocate memory for sealed data
 	sealedData := make([]byte, sealedSize)
 
 	// Seal the data
-	ret := C.seal_data(
-		(*C.uint8_t)(unsafe.Pointer(&testData[0])),
-		C.size_t(len(testData)),
+	ret = C.seal_data(
 		(*C.uint8_t)(unsafe.Pointer(&sealedData[0])),
-		C.size_t(sealedSize),
+		C.uint32_t(sealedSize),
 	)
 	if ret != 0 {
 		log.Fatalf("Failed to seal data: %d", ret)
@@ -41,9 +44,7 @@ func main() {
 	// Unseal the data
 	ret = C.unseal_data(
 		(*C.uint8_t)(unsafe.Pointer(&sealedData[0])),
-		C.size_t(sealedSize),
-		(*C.uint8_t)(unsafe.Pointer(&unsealedData[0])),
-		C.size_t(len(testData)),
+		C.uint32_t(sealedSize),
 	)
 	if ret != 0 {
 		log.Fatalf("Failed to unseal data: %d", ret)
