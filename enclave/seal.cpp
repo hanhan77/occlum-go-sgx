6331e65fdb9b89b13551dddf8c15896ea5f3d26f
Eunice <eunice.chen@conyft.com>
2025-06-06 16:22:46 +0800

    fix build 7

--- enclave/seal.cpp
+++ enclave/seal.cpp
@@ -12,10 +12,6 @@
 #define SEAL_TAG_SIZE 16
 
 // Calculate the size needed for sealed data
-size_t get_sealed_data_size(size_t data_size) {
-    return sgx_calc_sealed_data_size(0, data_size);
-}
-
 sgx_status_t get_sealed_data_size(const uint8_t *data, uint32_t data_size, uint32_t *sealed_data_size) {
     if (!data || !sealed_data_size) {
         return SGX_ERROR_INVALID_PARAMETER;
@@ -30,42 +26,25 @@ sgx_status_t get_sealed_data_size(const uint8_t *data, uint32_t data_size, uint3
 }
 
 // Seal data within the enclave
-sgx_status_t seal_data(const uint8_t* data, 
-                      size_t data_size,
-                      uint8_t* sealed_data,
-                      size_t sealed_size) {
-    if (!data || !sealed_data) {
-        return SGX_ERROR_INVALID_PARAMETER;
-    }
-
-    // Verify the sealed data size
-    size_t required_size = get_sealed_data_size(data_size);
-    if (sealed_size < required_size) {
+sgx_status_t seal_data(uint8_t* sealed_blob, uint32_t data_size) {
+    if (!sealed_blob) {
         return SGX_ERROR_INVALID_PARAMETER;
     }
 
     // Seal the data
-    return sgx_seal_data(0, NULL, data_size, data, sealed_size, (sgx_sealed_data_t*)sealed_data);
+    return sgx_seal_data(0, NULL, data_size, sealed_blob, data_size, (sgx_sealed_data_t*)sealed_blob);
 }
 
 // Unseal data within the enclave
-sgx_status_t unseal_data(const uint8_t* sealed_data,
-                        size_t sealed_size,
-                        uint8_t* data,
-                        size_t data_size) {
-    if (!sealed_data || !data) {
+sgx_status_t unseal_data(const uint8_t* sealed_blob, size_t data_size) {
+    if (!sealed_blob) {
         return SGX_ERROR_INVALID_PARAMETER;
     }
 
     // Get the size of the unsealed data
-    uint32_t mac_text_len = sgx_get_add_mac_txt_len((const sgx_sealed_data_t*)sealed_data);
-    uint32_t decrypted_text_len = sgx_get_encrypt_txt_len((const sgx_sealed_data_t*)sealed_data);
-
-    // Verify the data size
-    if (data_size < decrypted_text_len) {
-        return SGX_ERROR_INVALID_PARAMETER;
-    }
+    uint32_t mac_text_len = sgx_get_add_mac_txt_len((const sgx_sealed_data_t*)sealed_blob);
+    uint32_t decrypted_text_len = sgx_get_encrypt_txt_len((const sgx_sealed_data_t*)sealed_blob);
 
     // Unseal the data
-    return sgx_unseal_data((const sgx_sealed_data_t*)sealed_data, NULL, &mac_text_len, data, &decrypted_text_len);
+    return sgx_unseal_data((const sgx_sealed_data_t*)sealed_blob, NULL, &mac_text_len, sealed_blob, &decrypted_text_len);
 } 
--- enclave/seal.h
+++ enclave/seal.h
@@ -1,32 +1,24 @@
-#ifndef _SEAL_H_
-#define _SEAL_H_
+#ifndef SEAL_H
+#define SEAL_H
 
 #include <stdint.h>
-#include <stddef.h>
-#include <sgx_tcrypto.h>
-#include <sgx_trts.h>
+#include <sgx_error.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 // Calculate the size needed for sealed data
-size_t get_sealed_data_size(size_t data_size);
+sgx_status_t get_sealed_data_size(const uint8_t *data, uint32_t data_size, uint32_t *sealed_data_size);
 
 // Seal data within the enclave
-sgx_status_t seal_data(const uint8_t* data, 
-                      size_t data_size,
-                      uint8_t* sealed_data,
-                      size_t sealed_size);
+sgx_status_t seal_data(uint8_t* sealed_blob, uint32_t data_size);
 
 // Unseal data within the enclave
-sgx_status_t unseal_data(const uint8_t* sealed_data,
-                        size_t sealed_size,
-                        uint8_t* data,
-                        size_t data_size);
+sgx_status_t unseal_data(const uint8_t* sealed_blob, size_t data_size);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* _SEAL_H_ */ 
+#endif // SEAL_H 
